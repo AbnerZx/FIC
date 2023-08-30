8011a2996de169498d995790de0b5c9b6817654f
Abner Ferreira <abnerferreira3050@gmail.com>
2023-08-30 22:43:55 +0000

    New JS

---
+++ Linguagens de Programação/JavaScript/idade.js
@@ -0,0 +1,8 @@
+idade = parseInt(prompt('Diga qual a sua idade: '))
+
+if (idade > 17) {
+    alert('Você é maior de idade')
+}
+else{
+    alert('Você é menor de idade')
+}
---
+++ Linguagens de Programação/JavaScript/sorvete.js
@@ -0,0 +1,45 @@
+let sabor;
+
+do {
+    sabor = parseInt(prompt("Bem-vindo à sorveteria ice!\nPara sair digite 0!\nQual sabor você deseja?\n\n1- Chocolate\n2- Morango\n\nDigite uma opção: "));
+    if (sabor !== 0 && sabor !== 1 && sabor !== 2) {
+        alert("Opção inválida.\nApenas digite 1 para Chocolate ou 2 para Morango.");
+    }
+} while (sabor !== 0 && sabor !== 1 && sabor !== 2);
+
+let op;
+
+while (sabor !== 0) {
+    let bolas = parseInt(prompt("Informe a quantidade de bolas: "));
+    
+    while (isNaN(bolas) || bolas === "" || bolas === 0) {
+        bolas = parseInt(prompt("Você não informou a quantidade de bolas\nPor favor informe a quantidade de bolas"));
+    }
+    
+    if (sabor === 1) {
+        if (bolas > 3) {
+            alert("Você tem direito a um desconto de 10%.");
+        } else {
+            alert("Você tem direito a um desconto de 5%.");
+        }
+    } else {
+        alert("Sorvete sem desconto.");
+    }
+    
+    op = parseInt(prompt("Deseja fazer uma nova compra?\n\n1- Sim\n2- Não\n\nDigite uma opção:"));
+    while (op !== 1 && op !== 2) {
+        op = parseInt(prompt("Opção inválida\nDeseja fazer uma nova compra?\n\n1- Sim\n2- Não"));
+    }
+    
+    if (op === 1) {
+        do {
+            sabor = parseInt(prompt("Qual sabor você deseja?\n\n1- Chocolate\n2- Morango\n\nDigite uma opção: "));
+            if (sabor !== 0 && sabor !== 1 && sabor !== 2) {
+                alert("Opção inválida. Digite 1 para Chocolate, 2 para Morango.");
+            }
+        } while (sabor !== 0 && (sabor !== 1 && sabor !== 2));
+    } else {
+        alert("Sistema encerrado");
+        break; // Exit the loop
+    }
+}

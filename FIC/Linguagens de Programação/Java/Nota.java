5e2cdae3ccad0f9d57f15e4e2fe11a4d38ee4a51
Abner Ferreira <abnerferreira3050@gmail.com>
2023-09-11 23:17:15 +0000

    Update

--- FIC/Linguagens de Programação/Java/Nota.java
+++ FIC/Linguagens de Programação/Java/Nota.java
@@ -1,32 +1,29 @@
-// Importa a classe Scanner do pacote java.util, que permite a entrada de dados pelo usuário.
 import java.util.Scanner;
 
-// Declaração da classe "Programa".
 class Programa {
-    // Método principal do programa.
     public static void main(String[] args) {
-        // Declaração de variáveis para armazenar as notas e a média.
+        // Declaração das variáveis
         double nota1 = 0, nota2 = 0, media = 0;
 
-        // Criação de um objeto Scanner para ler a entrada do usuário.
+        // Início do bloco try-with-resources para criar e gerenciar o Scanner
         try (Scanner s = new Scanner(System.in)) {
+            // Solicita ao usuário que digite a primeira e a segunda nota
+            System.out.print("Digite a primeira e a segunda nota: ");
+            
+            // Lê a primeira nota fornecida pelo usuário e a armazena na variável nota1
+            nota1 = s.nextDouble();
+            
+            // Solicita ao usuário que digite a segunda nota
+            System.out.print("Digite a segunda nota: ");
+            
+            // Lê a segunda nota fornecida pelo usuário e a armazena na variável nota2
+            nota2 = s.nextDouble();
+        } // O bloco try é encerrado aqui, e o Scanner é fechado automaticamente
 
-        // Solicita ao usuário que digite as notas.
-        System.out.print("Digite a primeira e a segunda nota: ");
-        
-        // Lê a primeira nota digitada pelo usuário e a armazena na variável "nota1".
-        nota1 = s.nextDouble();
-        
-        // Solicita ao usuário que digite a segunda nota.
-        System.out.print("Digite a segunda nota: ");
-        
-        // Lê a segunda nota digitada pelo usuário e a armazena na variável "nota2".
-        nota2 = s.nextDouble();
-        }
-        // Calcula a média das duas notas.
+        // Calcula a média das notas
         media = (nota1 + nota2) / 2;
-
-        // Exibe a média calculada na tela.
+        
+        // Exibe a média calculada no console
         System.out.println("A média do aluno foi: " + media);
     }
 }

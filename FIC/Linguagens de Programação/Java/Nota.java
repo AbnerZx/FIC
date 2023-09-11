806967d110e23e4d52448af366c03936f67fda40
Abner Ferreira <abnerferreira3050@gmail.com>
2023-09-11 23:43:03 +0000

    Update

--- FIC/Linguagens de Programação/Java/Nota.java
+++ FIC/Linguagens de Programação/Java/Nota.java
@@ -5,25 +5,21 @@ class Programa {
         // Declaração das variáveis
         double nota1 = 0, nota2 = 0, media = 0;
 
-        // Início do bloco try-with-resources para criar e gerenciar o Scanner
-        try (Scanner s = new Scanner(System.in)) {
-            // Solicita ao usuário que digite a primeira e a segunda nota
-            System.out.print("Digite a primeira e a segunda nota: ");
-            
-            // Lê a primeira nota fornecida pelo usuário e a armazena na variável nota1
-            nota1 = s.nextDouble();
-            
-            // Solicita ao usuário que digite a segunda nota
-            System.out.print("Digite a segunda nota: ");
-            
-            // Lê a segunda nota fornecida pelo usuário e a armazena na variável nota2
-            nota2 = s.nextDouble();
-        } // O bloco try é encerrado aqui, e o Scanner é fechado automaticamente
+        Scanner s = new Scanner(System.in);
+        
+        // Solicita ao usuário que digite a primeira e a segunda nota e lê as notas
+        System.out.print("Digite a primeira nota: ");
+        nota1 = s.nextDouble();
+        System.out.print("Digite a segunda nota: ");
+        nota2 = s.nextDouble();
 
         // Calcula a média das notas
         media = (nota1 + nota2) / 2;
         
-        // Exibe a média calculada no console
-        System.out.println("A média do aluno foi: " + media);
+        // Exibe a média calculada no console usando printf
+        System.out.printf("A média do aluno foi: %.2f%n", media);
+
+        // Fechando o Scanner
+        s.close();
     }
 }

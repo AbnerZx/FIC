44f70f1cc13232aa3bd092d6d9ca3c0e2aa792b9
Abner Ferreira <abnerferreira3050@gmail.com>
2023-09-13 15:13:37 +0000

    Update

--- FIC/Linguagens de Programação/Java/Sorvete.java
+++ FIC/Linguagens de Programação/Java/Sorvete.java
@@ -2,57 +2,77 @@ import java.util.Scanner;
 
 class Sorvete {
     public static void main(String[] args) {
-        int sabor;
-        int bolas;
-        int opcao;
-        Scanner sabor1 = new Scanner(System.in);
-        Scanner bolas1 = new Scanner(System.in);
-        Scanner opcao1 = new Scanner(System.in);
-        System.out.print("Bem-vindo à sorveteria ice!\nPara sair digite 0!\nPor favor escolha um sabor:\n\n1- Chocolate\n2- Morango\n\nDigite uma opção: ");
-        sabor = sabor1.nextInt();
-        while (sabor != 0) {
-            while (sabor > 2 || sabor < 0) {
-                System.out.println("Opção inválida\nPor favor digite novamente: ");
-                sabor = sabor1.nextInt();
-            }
-            if (sabor == 0) {
-                System.out.println("Sistema encerrado!");
-                System.exit(0);
-            }
-            System.out.print("Informe a quantidade de bolas: ");
-            bolas = bolas1.nextInt();
-            while (bolas <=0) {
-                System.out.print("Quantidade inválida\nInforme a quantidade de bolas: ");
-                bolas = bolas1.nextInt();
-            }
-            if (sabor == 1) {
-                if (bolas > 3) {
-                    System.out.println("Você tem direito a um desconto de 10%");
+        int sabor = 0;
+        int bolas = 0;
+        int opcao = 0;
+
+        Scanner sorvete = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Bem-vindo à sorveteria ice!\nPara sair, digite 0!\nPor favor escolha um sabor:\n\n1- Chocolate\n2- Morango\n\nDigite uma opção: ");
+
+            try {
+                sabor = Integer.parseInt(sorvete.nextLine());
+
+                if (sabor == 0) {
+                    System.out.println("Sistema encerrado!");
+                    break;
+                } else if (sabor >= 1 && sabor <= 2) {
+                    System.out.print("Informe a quantidade de bolas: ");
+                    
+                    while (true) {
+                        try {
+                            bolas = Integer.parseInt(sorvete.nextLine());
+
+                            if (bolas > 0) {
+                                break; // Sair do loop se a quantidade for válida
+                            } else {
+                                System.out.print("Quantidade inválida. Por favor, digite novamente: ");
+                            }
+                        } catch (NumberFormatException e) {
+                            System.out.print("Quantidade inválida. Por favor, digite novamente: ");
+                        }
+                    }
+
+                    if (sabor == 1) {
+                        if (bolas > 3) {
+                            System.out.println("Você tem direito a um desconto de 10%");
+                        } else {
+                            System.out.println("Você tem direito a um desconto de 5%");
+                        }
+                    } else {
+                        System.out.println("Sorvete sem desconto");
+                    }
+
+                    System.out.print("Muito obrigado pela sua compra\nDeseja fazer uma nova compra?\n\n1- SIm\n2- Não\n\nDigite uma opção: ");
+                    
+                    while (true) {
+                        try {
+                            opcao = Integer.parseInt(sorvete.nextLine());
+
+                            if (opcao == 1 || opcao == 2) {
+                                break; // Sair do loop se a opção for válida
+                            } else {
+                                System.out.print("Opção inválida. Por favor, digite novamente: ");
+                            }
+                        } catch (NumberFormatException e) {
+                            System.out.print("Opção inválida. Por favor, digite novamente: ");
+                        }
+                    }
+
+                    if (opcao == 1) {
+                        continue; // Continue para a próxima iteração do loop
+                    } else {
+                        System.out.println("Sistema encerrado!");
+                        break;
+                    }
+                } else {
+                    System.out.print("Opção inválida. Por favor, digite novamente:\n");
                 }
-                else {
-                    System.out.println("Você tem direito a um desconto de 5%");
-                }
-            }
-            else {
-                System.out.println("Sorvete sem desconto");
-            }
-            System.out.print("Muito obrigado pela sua compra\nDeseja fazer uma nova compra?\n\n1- SIm\n2- Não\n\nDigite uma opção: ");
-            opcao = opcao1.nextInt();
-            while (opcao > 2 || opcao <= 0) {
-                System.out.println("Opção inválida\nDeseja fazer uma nova compra?\n\n1- Sim\n2- Não\n\nDigite uma opção válida: ");
-                opcao = opcao1.nextInt();
-            }
-            if (opcao == 1) {
-                System.out.print("Olá novamente!\nPara sair digite 0!\nPor favor escolha um sabor:\n\n1- Chocolate\n2- Morango\n\nDigite uma opção: ");
-                sabor = sabor1.nextInt();
-            }
-            else {
-                System.out.println("Sistema encerrado!");
-                System.exit(0);
+            } catch (NumberFormatException e) {
+                System.out.print("Opção inválida. Por favor, digite novamente:\n");
             }
         }
-        sabor1.close();
-        bolas1.close();
-        opcao1.close();
+        sorvete.close();
     }
 }

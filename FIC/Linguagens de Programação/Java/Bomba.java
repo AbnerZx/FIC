3511227b41e6a2821713eeb91019f4b4c2c1b789
Abner Ferreira <abnerferreira3050@gmail.com>
2023-09-15 20:49:47 +0000

    New exercise

---
+++ FIC/Linguagens de Programação/Java/Bomba.java
@@ -0,0 +1,13 @@
+class Bomba {
+    public static void main(String[] args) throws InterruptedException {
+        int tempo = 10;
+        for (int c = tempo; c >= 0; c = c - 1) {
+            System.out.println("Bomba em " + c + " segundos");
+            if (c == 0) {
+                System.out.println("BOOOMMM!!!!!");
+            } else {
+                Thread.sleep(1000);
+            }
+        }
+    }
+}
